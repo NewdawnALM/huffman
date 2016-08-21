88c13da029582848440a130575877cc4b6d6245a
NewdawnALM <NewdawnALM@github.com>
2016-08-21 21:13:20 +0800

    读写文件函数

---
+++ File.h
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <string>
+using std::string;
+
+inline string readFile(const char *const file) {
+	FILE *fp = fopen(file, "rb+");
+	string str = "";
+	do {
+		unsigned int ch = fgetc(fp);
+		str.push_back(ch);
+	} while(!feof(fp));
+	fclose(fp);
+	return str;
+}
+
+inline void writeFile(const char *const file, const string &str) {
+	FILE *fp = fopen(file, "wb+");
+	for(auto it = str.begin(); it != str.end(); ++it) {
+        if(*it != EOF)  fputc(*it, fp);
+	}
+	fclose(fp);
+}
